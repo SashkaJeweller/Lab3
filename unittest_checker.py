5e4e07817b589412efb0c7afe8a4f6aeca1c73a0
SashkaJeweller <166250972+SashkaJeweller@users.noreply.github.com>
2024-12-20 15:04:08 +0300

    Update unittest_checker.py

--- unittest_checker.py
+++ unittest_checker.py
@@ -5,20 +5,18 @@ class TestRomanNumerals(unittest.TestCase):
     def test_valid_romans(self):
         valid_romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M", "MM", "MMM"]
         for roman in valid_romans:
-            with self.subTest(roman=roman):  # Использование subTest для улучшения вывода ошибок
+            with self.subTest(roman=roman): 
                 self.assertTrue(is_valid_roman(roman))
 
     def test_invalid_romans(self):
         invalid_romans = ["IIII", "VV", "XXXX", "IL", "IC", "MCMXCIVV", "MMMM"]
         for roman in invalid_romans:
-            with self.subTest(roman=roman):  # Использование subTest для улучшения вывода ошибок
+            with self.subTest(roman=roman): 
                 self.assertFalse(is_valid_roman(roman))
 
 if __name__ == "__main__":
-    # Запускаем тесты и сохраняем результат
-    result = unittest.main(exit=False)
 
-    # Проверяем, прошли ли все тесты
+    result = unittest.main(exit=False)
     if result.result.wasSuccessful():
         print("\nВсе тесты прошли успешно!")
     else:
